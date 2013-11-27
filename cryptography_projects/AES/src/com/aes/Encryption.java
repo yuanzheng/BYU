4e4f250e2e218287a5bbda21504d0eea67767b92
Yuanzheng Lucas Song <yuanzheng@cs.byu.edu>
2013-11-27 01:24:20 -0700

    create AES project

---
+++ cryptography_projects/AES/src/com/aes/Encryption.java
@@ -0,0 +1,49 @@
+package com.aes;
+
+/*
+ * Encryption
+ * @author Song, Yuanzheng
+ * 
+ */
+
+public class Encryption implements CipherInterface {
+	
+	//XOR
+	private byte ffAdd(byte a, byte b){
+		
+		return (byte)(a^b);
+	}
+	
+	//[SY] testing xtime
+	private byte xtime(byte a){
+		byte out = (byte)(a<<1);
+		
+		if(a<0)
+			return (byte)(out^((byte)0x1b));
+		else
+			return out;	
+	}
+	
+	private byte ffMultiply(byte xL, byte yR){
+		
+		byte result = xL;
+		//byte a = 0;
+		byte b = 0;
+		
+		for(int i=0; i<8; i++)
+		{
+			if((yR & 1) == 1)  // check if the first bit is 1
+			{
+				b = (byte)(b ^ result);
+			}
+			
+			result = xtime(result);
+			yR = (byte)(yR>>1);
+		}
+		
+		return b;
+	}
+	
+	
+
+}
