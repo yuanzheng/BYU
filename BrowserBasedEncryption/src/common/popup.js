d45e9be1d088f3a48db2e9d3f8ec8e9d00059d7d
Yuanzheng Lucas Song <yuanzheng@cs.byu.edu>
2014-01-01 18:20:44 -0700

    BME user interface is created

--- BrowserBasedEncryption/src/common/popup.js
+++ BrowserBasedEncryption/src/common/popup.js
@@ -7,7 +7,49 @@
  */
 
 // Save this script as `options.js`
+$(function() {
+	$( "#datepicker" ).datepicker();
+});
 
+$(document).ready(function()
+{
+	$(".account").click(function()
+{
+var X=$(this).attr('id');
+
+if(X==1)
+{
+	$(".submenu").hide();
+	$(this).attr('id', '0');	
+}
+else
+{
+	$(".submenu").show();
+	$(this).attr('id', '1');
+}
+	
+});
+
+//Mouseup textarea false
+$(".submenu").mouseup(function()
+{
+	return false
+});
+$(".account").mouseup(function()
+{
+	return false
+});
+
+
+//Textarea without editing.
+$(document).mouseup(function()
+{
+	$(".submenu").hide();
+	$(".account").attr('id', '');
+});
+	
+});
+	
 // Saves current content into localStorage.
 function save_options() {
     var content = document.getElementById("input").value;
